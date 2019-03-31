c11b5e6d120fd54e915c16c1dc4468cfdcbe79e4
Camilo <camilo.antonio.t@gmail.com>
2019-03-31 17:55:58 -0300

    Creando mutation e input para nuevos clientes

--- GraphQL-Servidor/index.js
+++ GraphQL-Servidor/index.js
@@ -7,45 +7,68 @@ import graphqlHTTP from 'express-graphql';
 // Se importa schema
 import schema from './schema';
 
-///////////////////////////////////////////////////////////////////
-// Express es una infraestructura web de direccionamiento y 
-// middleware que tiene una funcionalidad mínima propia: una 
-// aplicación Express es fundamentalmente una serie de llamadas a 
-// funciones de middleware.
-///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+// Express es una infraestructura web de direccionamiento y        //
+// middleware que tiene una funcionalidad mínima propia: una       //
+// aplicación Express es fundamentalmente una serie de llamadas a  //
+// funciones de middleware.                                        //
+/////////////////////////////////////////////////////////////////////
 // Se crear una constante del objeto express
 const app = express();
 
-///////////////////////////////////////////////////////////////////
-// Las funciones de middleware son funciones que 
-// tienen acceso al objeto de solicitud (req), al objeto de 
-// respuesta (res) y a la siguiente(next) función de middleware en 
-// el ciclo de solicitud/respuestas de la aplicación. 
-///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+// Las funciones de middleware son funciones que tiene acceso al   //
+// objeto de solicitud (req), al objeto de respuesta (res) y a la  //
+// siguiente(next) función de middleware en el ciclo de            //
+// solicitud / respuestas de la aplicación.                        //
+/////////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
     res.send('Todo Listo');
 });
 
+// Se crear una clase cliente con un contructor con parametros
+class Cliente{
+    constructor(id, {nombre, apellido, empresa, email}){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.empresa = empresa;
+        this.email = email;
+    }
+}
+
+// Emula un DB, la constante es igual a un objeto vacio.
+const clientesDB = {};
+
+
 // El Resolver
-const root = {cliente: () => {
+const root = {
+    cliente: () => {
         return {
             "id": 12345678,
             "nombre": "Aquiles",
             "apellido": "Bailo",
             "empresa": "DDR",
-            "emails": [
-                {email: "aquiles.bailo@gmail.com"},
-                {email: "a.bailo@gmail.com"}
-            ]
+            "email": "aquiles.bailo@gmail.com"
         }
+    },
+    crearCliente: ({input}) => {
+        const id = require('crypto').randomBytes(10).toString('hex');
+        // array (id => input)
+        clientesDB[id] = input;
+        //////////////////////////////////////////////////////////////
+        // Se retorna una nueva instacia de cliente, el cual recibe //
+        // por parametro el id y el object input                    //
+        //////////////////////////////////////////////////////////////
+        return new Cliente(id, input);
     }
 };
 
-//////////////////////////////////////////////////////////////////////
-// Este ejemplo muestra una función de middleware sin ninguna vía de 
-// acceso de montaje. La función se ejecuta para cualquier tipo de 
-// solicitud HTTP: (por ejemplo, GET, PUT o POST)
-//////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+// Este ejemplo muestra una función de middleware sin ninguna vía de //
+// acceso de montaje. La función se ejecuta para cualquier tipo de   //
+// solicitud HTTP: (por ejemplo, GET, PUT o POST)                    //
+///////////////////////////////////////////////////////////////////////
 
 // Nueva ruta para acceder a graphiql
 app.use('/graphql', graphqlHTTP({
